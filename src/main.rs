31f6b85190af0e50013f1e132f0989ca0db15a12
juhotuho10 <juhotuho10@gmail.com>
2025-02-05 01:25:55 +0200

    simple blinky setup

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,39 @@
 #![no_std]
 #![no_main]
 
-use esp_backtrace as _;
-use esp_hal::{delay::Delay, prelude::*};
+use esp32_hal::{
+    clock::ClockControl, entry, gpio::IO, peripherals::Peripherals, prelude::*, timer::TimerGroup,
+    Delay, Rtc,
+};
+
+//use esp32_hal::xtensa_lx_rt::entry; ??
+use panic_halt as _;
 
 #[entry]
 fn main() -> ! {
-    #[allow(unused)]
-    let peripherals = esp_hal::init(esp_hal::Config::default());
-    let delay = Delay::new();
+    let peripherals = Peripherals::take();
+    let mut system = peripherals.DPORT.split();
+    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
 
-    esp_println::logger::init_logger_from_env();
+    let timer_group0 = TimerGroup::new(
+        peripherals.TIMG0,
+        &clocks,
+        &mut system.peripheral_clock_control,
+    );
+    let mut wdt = timer_group0.wdt;
+    let mut rtc = Rtc::new(peripherals.RTC_CNTL);
+    wdt.disable();
+    rtc.rwdt.disable();
+
+    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+    let mut led = io.pins.gpio2.into_push_pull_output();
+
+    let mut delay = Delay::new(&clocks);
 
     loop {
-        log::info!("Hello world!");
-        delay.delay(500.millis());
+        led.set_high().unwrap();
+        delay.delay_ms(3000u32);
+        led.set_low().unwrap();
+        delay.delay_ms(3000u32);
     }
 }
