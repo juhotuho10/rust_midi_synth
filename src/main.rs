77e0ddc64eb4470cc95cf8846e39e51b7fde6252
juhotuho10 <juhotuho10@gmail.com>
2025-02-05 18:30:38 +0200

    implemented rotary encoder functionality on simple leds

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,33 @@ use esp32_hal::{
     clock::ClockControl, gpio::IO, peripherals::Peripherals, timer::TimerGroup, Delay, Rtc,
 };
 
+enum Rotation {
+    Left,
+    Right,
+}
+
+fn get_knob_rotation(
+    last_clk: bool,
+    last_dt: bool,
+    current_clk: bool,
+    current_dt: bool,
+) -> Option<Rotation> {
+    match (last_clk, last_dt) {
+        (true, true) => match (current_clk, current_dt) {
+            (true, false) => return Some(Rotation::Left),
+            (false, true) => return Some(Rotation::Right),
+            (_, _) => return None,
+        },
+
+        (false, false) => match (current_clk, current_dt) {
+            (false, true) => return Some(Rotation::Left),
+            (true, false) => return Some(Rotation::Right),
+            (_, _) => return None,
+        },
+        (_, _) => return None,
+    }
+}
+
 #[entry]
 fn main() -> ! {
     let peripherals = Peripherals::take();
@@ -26,22 +53,53 @@ fn main() -> ! {
     wdt.disable();
     rtc.rwdt.disable();
 
+    let mut delay = Delay::new(&clocks);
+
     // ---------- set up pins ----------
 
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
 
+    let mut pin0 = io.pins.gpio0.into_push_pull_output();
     let mut led = io.pins.gpio2.into_push_pull_output();
 
-    let mut pin0 = io.pins.gpio0.into_push_pull_output();
+    // roatry encoder input pins
+    let clk = io.pins.gpio26.into_pull_up_input();
+    let dt = io.pins.gpio1.into_pull_up_input();
+    let sw = io.pins.gpio3.into_pull_up_input();
 
-    let mut delay = Delay::new(&clocks);
+    // last states for rotary encode pins
+    let mut last_clk_state = clk.is_high().unwrap();
+    let mut last_dt_state = dt.is_high().unwrap();
+    let mut last_sw_state = sw.is_low().unwrap();
 
     loop {
-        led.set_high().unwrap();
-        pin0.set_high().unwrap();
-        delay.delay_ms(300u32);
-        led.set_low().unwrap();
-        pin0.set_low().unwrap();
-        delay.delay_ms(300u32);
+        // current states
+        let current_clk_state = clk.is_high().unwrap();
+        let current_dt_state = dt.is_high().unwrap();
+        let current_sw_state = sw.is_low().unwrap();
+
+        // pin logic
+        if sw.is_low().unwrap() && current_sw_state != last_sw_state {
+            led.toggle().unwrap();
+        }
+
+        if let Some(rotation) = get_knob_rotation(
+            last_clk_state,
+            last_dt_state,
+            current_clk_state,
+            current_dt_state,
+        ) {
+            match rotation {
+                Rotation::Left => pin0.set_low().unwrap(),
+                Rotation::Right => pin0.set_high().unwrap(),
+            }
+        }
+
+        // reset current states
+        last_dt_state = current_dt_state;
+        last_sw_state = current_sw_state;
+        last_clk_state = current_clk_state;
+
+        delay.delay_ms(5u32);
     }
 }
